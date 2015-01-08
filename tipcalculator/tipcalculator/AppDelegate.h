867c08fc1f9c2ddca5761265a4ab5ae52bb51edd
Gaurav Gargate <ggargate@box.com>
2015-01-08 07:41:50 -0800

    Initial commit of the tip calculator Uses slider

---
+++ tipcalculator/tipcalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Gaurav Gargate on 1/2/15.
+//  Copyright (c) 2015 Gaurav Gargate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
